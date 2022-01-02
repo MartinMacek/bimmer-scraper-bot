8fd5fec10e368198f2a14206fd83adffff88bce6
Martin Macek <martinmacek.dev@gmail.com>
2022-01-02 12:42:56 +0100

    feat: send telegram message

--- bin/bimmer_scraper_bot.dart
+++ bin/bimmer_scraper_bot.dart
@@ -1,10 +1,37 @@
+import 'dart:async';
+
+import 'package:dotenv/dotenv.dart';
+import 'package:teledart/teledart.dart';
+import 'package:teledart/telegram.dart';
 import 'package:web_scraper/web_scraper.dart';
 
 void main(List<String> arguments) async {
-  final webScraper = WebScraper('https://webscraper.io');
-  if (await webScraper.loadWebPage('/test-sites/e-commerce/allinone')) {
-    List<Map<String, dynamic>> elements =
-        webScraper.getElement('h3.title > a.caption', ['href']);
-    print(elements);
-  }
+  print("bot started...");
+  load();
+  final searchParams =
+      '/inzerce/osobni/bmw?cena-do=500000&km-do=150000&typ=kombi&prevodovka=automaticka&pohon=4x4';
+  final tray = [];
+  final telegram = Telegram(env['botToken'] as String);
+  final event = Event((await telegram.getMe()).username!);
+  const period = Duration(minutes: 1);
+  final webScraper = WebScraper('https://www.sauto.cz');
+  var isInitialRun = true;
+
+  TeleDart(telegram, event).start();
+
+  Timer.periodic(period, (Timer t) async {
+    if (await webScraper.loadWebPage(searchParams)) {
+      List<Map<String, dynamic>> elements =
+          webScraper.getElement('a.sds-surface', ['href']);
+
+      for (dynamic element in elements) {
+        final href = element["attributes"]["href"];
+        if (!tray.contains(href)) {
+          tray.add(href);
+          if (!isInitialRun) telegram.sendMessage(env['chatId'], href);
+        }
+      }
+    }
+    isInitialRun = false;
+  });
 }
